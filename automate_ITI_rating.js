ad0eb186e3f81451e09eda29d5c554e235ae4d63
rjOwl <mostafayossef100@yahoo.com>
2021-01-18 12:07:23 +0200

    Fixed getelementbyid is null

---
+++ automate_ITI_rating.js
@@ -0,0 +1,20 @@
+var event = new MouseEvent('mouseover', {
+  'view': window,
+  'bubbles': true,
+  'cancelable': true
+});
+
+
+
+for(let i = 0; i<7; i++){
+  for(let j = 0; j < 15;j++){
+        for(let star = 1; star<4; star++){
+document.getElementById(`ContentPlaceHolder1_UcCourseEval1_DataListLapEval_GrdViewlabEval_${i}_Rating1_${j}_Star_${star}`).dispatchEvent(event);
+}
+}
+}
+for(let j = 0; j < 15;j++){
+        for(let star = 1; star<4; star++){
+document.getElementById(`ContentPlaceHolder1_UcCourseEval1_GrdViewcourseEval_Rating1_${j}_Star_${star}`).dispatchEvent(event);
+}
+}
