0a4884b9ff13478e0809e93513c7181f4490e964
rjOwl <mostafayossef100@yahoo.com>
2021-01-19 04:22:41 +0200

    Splitting code into functions
    Explaining the structure of the divs in the page

--- automate_rating.js
+++ automate_rating.js
@@ -1,24 +1,67 @@
-var event = new MouseEvent('mouseover', {
-  'view': window,
-  'bubbles': true,
-  'cancelable': true
-});
+STARS = 5
+TA_CRITERIA = 15
+OVERALL_CRITERIA = 9
+course = {
+  lecture:{
+    criteria_num: TA_CRITERIA,
+    stars_id: "ContentPlaceHolder1_UcCourseEval1_DataListLectureEval_GrdViewinstEval_0_Rating1_${criteria}_Star_${star}",
+    hidden_id: "ContentPlaceHolder1_UcCourseEval1_DataListLectureEval_GrdViewinstEval_0_Rating1_RatingExtender_ClientState_${criteria}",
+  },
+  lab:{
+    instructors: parseInt(document.getElementById("ContentPlaceHolder1_UcCourseEval1_DataListLapEval").rows.length),
+    criteria_num: TA_CRITERIA,
+    stars_id: "ContentPlaceHolder1_UcCourseEval1_DataListLectureEval_GrdViewinstEval_0_Rating1_${criteria}_Star_${star}",
+    hidden_id: "ContentPlaceHolder1_UcCourseEval1_DataListLapEval_GrdViewlabEval_${instructor}_Rating1_RatingExtender_ClientState_${criteria}",
+  },
+  overall:{
+    criteria_num: OVERALL_CRITERIA,
+    stars_id: "ContentPlaceHolder1_UcCourseEval1_GrdViewcourseEval_Rating1_${criteria}_Star_${star}",
+    hidden_id: "ContentPlaceHolder1_UcCourseEval1_GrdViewcourseEval_Rating1_RatingExtender_ClientState_${criteria}"
+  }
+}
 
 
-
-for(let i = 0; i<7; i++){
-  for(let j = 0; j < 15;j++){
-        for(let star = 1; star<4; star++){
-document.getElementById(`ContentPlaceHolder1_UcCourseEval1_DataListLapEval_GrdViewlabEval_${i}_Rating1_${j}_Star_${star}`).dispatchEvent(event);
-}
-}
-}
-for(let j = 0; j < 15;j++){
-        for(let star = 1; star<4; star++){
-document.getElementById(`ContentPlaceHolder1_UcCourseEval1_GrdViewcourseEval_Rating1_${j}_Star_${star}`).dispatchEvent(event);
-}
+function instructor_rating(lecture, value, overallStars=5){
+    for(let criteria = 0; criteria< TA_CRITERIA; criteria++){
+      let id = lecture.hidden_id.replace("${criteria}", criteria)
+      document.getElementById(id).setAttribute("value", value);
+    }
 }
 
-$('#target').on('click mouseover', function () {
-    // Do something for both
-});
+function lab_rating(lab, value, overallStars=5){
+  for(let instructor = 0; instructor<lab.instructors; instructor++){
+    for(let criteria = 0; criteria< TA_CRITERIA; criteria++){
+      let id = lab.hidden_id.replace("${instructor}", instructor)
+      id = id.replace("${criteria}", criteria)
+      document.getElementById(id).setAttribute("value", value);
+    }
+  }
+}
+
+function overall_rating(overall, value, overallStars=5){
+  for(let criteria = 0; criteria< TA_CRITERIA; criteria++){
+    let id = overall.hidden_id.replace("${criteria}", criteria)
+    document.getElementById(id).setAttribute("value", value);
+  }
+}
+
+
+// Structure
+// =========
+// Lecturer
+"ContentPlaceHolder1_UcCourseEval1_DataListLectureEval_GrdViewinstEval_0_Rating1_RatingExtender_ClientState_0"
+"ContentPlaceHolder1_UcCourseEval1_DataListLectureEval_GrdViewinstEval_0_Rating1_RatingExtender_ClientState_${criteria}"
+
+// Lab Instructors Eval
+// Instructor 3
+  "ContentPlaceHolder1_UcCourseEval1_DataListLapEval_GrdViewlabEval_${instructor}_Rating1_RatingExtender_ClientState_{criteria}"
+  "ContentPlaceHolder1_UcCourseEval1_DataListLapEval_GrdViewlabEval_3_Rating1_RatingExtender_ClientState_1"
+// Instructor 4
+  "ContentPlaceHolder1_UcCourseEval1_DataListLapEval_GrdViewlabEval_4_Rating1_RatingExtender_ClientState_0"
+  "ContentPlaceHolder1_UcCourseEval1_DataListLapEval_GrdViewlabEval_4_Rating1_RatingExtender_ClientState_1"
+
+// Course Eval
+"ContentPlaceHolder1_UcCourseEval1_GrdViewcourseEval_Rating1_RatingExtender_ClientState_0"
+"ContentPlaceHolder1_UcCourseEval1_GrdViewcourseEval_Rating1_RatingExtender_ClientState_1"
+
+
