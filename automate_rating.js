77927e7f69dfe36440519b840ca67ef505cdf044
rjOwl <mostafayossef100@yahoo.com>
2021-01-19 05:17:15 +0200

    Creating class Automate and a main function

--- automate_rating.js
+++ automate_rating.js
@@ -1,50 +1,81 @@
-STARS = 5
-TA_CRITERIA = 15
-OVERALL_CRITERIA = 9
-course = {
-  lecture:{
-    criteria_num: TA_CRITERIA,
-    stars_id: "ContentPlaceHolder1_UcCourseEval1_DataListLectureEval_GrdViewinstEval_0_Rating1_${criteria}_Star_${star}",
-    hidden_id: "ContentPlaceHolder1_UcCourseEval1_DataListLectureEval_GrdViewinstEval_0_Rating1_RatingExtender_ClientState_${criteria}",
-  },
-  lab:{
-    instructors: parseInt(document.getElementById("ContentPlaceHolder1_UcCourseEval1_DataListLapEval").rows.length),
-    criteria_num: TA_CRITERIA,
-    stars_id: "ContentPlaceHolder1_UcCourseEval1_DataListLectureEval_GrdViewinstEval_0_Rating1_${criteria}_Star_${star}",
-    hidden_id: "ContentPlaceHolder1_UcCourseEval1_DataListLapEval_GrdViewlabEval_${instructor}_Rating1_RatingExtender_ClientState_${criteria}",
-  },
-  overall:{
-    criteria_num: OVERALL_CRITERIA,
-    stars_id: "ContentPlaceHolder1_UcCourseEval1_GrdViewcourseEval_Rating1_${criteria}_Star_${star}",
-    hidden_id: "ContentPlaceHolder1_UcCourseEval1_GrdViewcourseEval_Rating1_RatingExtender_ClientState_${criteria}"
-  }
-}
+var event = new MouseEvent("mouseover", {
+  view: window,
+  bubbles: true,
+  cancelable: true,
+});
 
-
-function instructor_rating(lecture, value, overallStars=5){
-    for(let criteria = 0; criteria< TA_CRITERIA; criteria++){
-      let id = lecture.hidden_id.replace("${criteria}", criteria)
-      document.getElementById(id).setAttribute("value", value);
+class Automate{
+  constructor(){
+    this.STARS = 5
+    this.TA_CRITERIA = 15
+    this.OVERALL_CRITERIA = 9
+    this.course = {
+      lecture:{
+        criteria_num: this.TA_CRITERIA,
+        stars_id: "ContentPlaceHolder1_UcCourseEval1_DataListLectureEval_GrdViewinstEval_0_Rating1_${criteria}_Star_${star}",
+        hidden_id: "ContentPlaceHolder1_UcCourseEval1_DataListLectureEval_GrdViewinstEval_0_Rating1_RatingExtender_ClientState_${criteria}",
+      },
+      lab:{
+        check_instructors: document.getElementById("ContentPlaceHolder1_UcCourseEval1_DataListLapEval") === null,
+        instructors: 0,
+        criteria_num: this.TA_CRITERIA,
+        stars_id: "ContentPlaceHolder1_UcCourseEval1_DataListLectureEval_GrdViewinstEval_0_Rating1_${criteria}_Star_${star}",
+        hidden_id: "ContentPlaceHolder1_UcCourseEval1_DataListLapEval_GrdViewlabEval_${instructor}_Rating1_RatingExtender_ClientState_${criteria}",
+      },
+      overall:{
+        criteria_num: this.OVERALL_CRITERIA,
+        stars_id: "ContentPlaceHolder1_UcCourseEval1_GrdViewcourseEval_Rating1_${criteria}_Star_${star}",
+        hidden_id: "ContentPlaceHolder1_UcCourseEval1_GrdViewcourseEval_Rating1_RatingExtender_ClientState_${criteria}"
+      }
     }
-}
+    if(!this.course.lab.check_instructors)
+    this.course.lab = parseInt(document.getElementById("ContentPlaceHolder1_UcCourseEval1_DataListLapEval").rows.length)
+  }
+  instructor_rating(value, overallStars=5){
+      let lecture = this.course.lecture
+      for(let criteria = 0; criteria< lecture.criteria_num; criteria++){
+        let hidden_id = lecture.hidden_id.replace("${criteria}", criteria)
+        document.getElementById(hidden_id).setAttribute("value", value);
+        let stars_id = lecture.stars_id.replace("${criteria}", criteria)
+        stars_id = stars_id.replace("${star}", value)
+        document.getElementById(stars_id).dispatchEvent(event);
+      }
+  }
 
-function lab_rating(lab, value, overallStars=5){
-  for(let instructor = 0; instructor<lab.instructors; instructor++){
-    for(let criteria = 0; criteria< TA_CRITERIA; criteria++){
-      let id = lab.hidden_id.replace("${instructor}", instructor)
-      id = id.replace("${criteria}", criteria)
+  lab_rating(value, overallStars=5){
+    let lab = this.course.lab
+    for(let instructor = 0; instructor<lab.instructors; instructor++){
+      for(let criteria = 0; criteria< lab.criteria_num; criteria++){
+        let id = lab.hidden_id.replace("${instructor}", instructor)
+        id = id.replace("${criteria}", criteria)
+        document.getElementById(id).setAttribute("value", value);
+        let stars_id = lecture.stars_id.replace("${criteria}", criteria)
+        stars_id = stars_id.replace("${star}", value)
+        document.getElementById(stars_id).dispatchEvent(event);
+      }
+    }
+  }
+
+  overall_rating(value, overallStars=5){
+    let overall = this.course.overall
+    for(let criteria = 0; criteria< overall.criteria_num; criteria++){
+      let id = overall.hidden_id.replace("${criteria}", criteria)
       document.getElementById(id).setAttribute("value", value);
+      let stars_id = overall.stars_id.replace("${criteria}", criteria)
+      stars_id = stars_id.replace("${star}", value)
+      document.getElementById(stars_id).dispatchEvent(event);
     }
   }
 }
 
-function overall_rating(overall, value, overallStars=5){
-  for(let criteria = 0; criteria< TA_CRITERIA; criteria++){
-    let id = overall.hidden_id.replace("${criteria}", criteria)
-    document.getElementById(id).setAttribute("value", value);
-  }
-}
 
+function main(rate){
+  auto = new Automate()
+  auto.instructor_rating(rate)
+  auto.lab_rating(rate)
+  auto.overall_rating(rate)
+}
+main(5)
 
 // Structure
 // =========
